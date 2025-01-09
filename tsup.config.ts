83062e72fedb6feb6ea3672c274c3f2c86c60cb5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-01-09 14:01:26 -0500

    chore: fix build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: [
-    'src/index.ts',
-    'src/react/index.ts',
-    'src/vite/plugin.ts',
-    'src/transform/index.ts',
-    'src/runtime/index.ts',
-  ],
-  tsconfig: 'src/runtime/tsconfig.json',
-  format: ['esm'],
-  dts: true,
-})
+export default defineConfig([
+  {
+    entry: ['src/index.ts', 'src/react/index.ts', 'src/runtime/index.ts'],
+    tsconfig: 'src/runtime/tsconfig.json',
+    format: ['esm'],
+    dts: true,
+  },
+  {
+    entry: ['src/vite/plugin.ts', 'src/transform/index.ts'],
+    tsconfig: 'src/transform/tsconfig.json',
+    format: ['esm'],
+    dts: true,
+  },
+])
