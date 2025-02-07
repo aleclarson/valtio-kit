fc78a452068076a96aa2e5e16daf6df03f5a6c60
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-07 13:07:26 -0500

    fix(transform): some bug with debug transform
    Switching from typescript-eslint's internal simpleTraverse function to our fork (traverse) seems to have fixed it.

--- src/transform/debug.ts
+++ src/transform/debug.ts
@@ -1,10 +1,10 @@
 import {
   parse,
-  simpleTraverse,
   AST_NODE_TYPES as T,
 } from '@typescript-eslint/typescript-estree'
 import MagicString from 'magic-string'
 import { dedent } from 'radashi'
+import { traverse } from './utils/traverse'
 
 /**
  * Modify Valtio at compile-time to inject debug hooks.
@@ -18,48 +18,65 @@ export function applyDebugTransform(code: string) {
 
   const result = new MagicString(code)
 
-  simpleTraverse(ast, {
-    enter(node) {
-      if (
-        node.type === T.FunctionDeclaration &&
-        node.id?.type === T.Identifier &&
-        node.id.name === 'proxy'
-      ) {
-        simpleTraverse(node, {
-          enter(node) {
-            // Inject the `globalThis.valtioHook` call into the `notifyUpdate`
-            // function.
-            if (
-              node.type === T.VariableDeclarator &&
-              node.id.type === T.Identifier &&
-              node.id.name === 'notifyUpdate' &&
-              node.init?.type === T.ArrowFunctionExpression
-            ) {
-              const functionBody = node.init.body
-              if (functionBody.type === T.BlockStatement) {
-                result.prependLeft(
-                  functionBody.range[0] + 1,
-                  `globalThis.valtioHook?.('update', baseObject, op, listeners);`
-                )
-              }
-            }
+  const proxyFunction = ast.body.find(
+    node =>
+      node.type === T.FunctionDeclaration &&
+      node.id?.type === T.Identifier &&
+      node.id.name === 'proxy'
+  )
 
-            // Wrap the proxy handler with a `trackMethodCalls` call.
-            if (
-              node.type === T.VariableDeclarator &&
-              node.id.type === T.Identifier &&
-              node.id.name === 'handler' &&
-              node.init
-            ) {
-              const initializer = node.init
-              result.prependLeft(initializer.range[0], 'trackMethodCalls(')
-              result.appendRight(initializer.range[1], ', baseObject)')
-            }
-          },
-        })
-      }
+  if (!proxyFunction) {
+    console.warn('[valtio-kit] Could not find `proxy` function')
+    return null
+  }
+
+  let notifyUpdateFound = false
+  let proxyHandlerFound = false
+
+  traverse(proxyFunction, {
+    visitors: {
+      VariableDeclarator(node) {
+        if (node.id.type !== T.Identifier) {
+          return
+        }
+
+        // Inject the `globalThis.valtioHook` call into the `notifyUpdate`
+        // function.
+        if (
+          node.id.name === 'notifyUpdate' &&
+          node.init?.type === T.ArrowFunctionExpression
+        ) {
+          const functionBody = node.init.body
+          if (functionBody.type === T.BlockStatement) {
+            result.prependLeft(
+              functionBody.range[0] + 1,
+              `globalThis.valtioHook?.('update', baseObject, op, listeners);`
+            )
+            notifyUpdateFound = true
+          }
+          return
+        }
+
+        // Wrap the proxy handler with a `trackMethodCalls` call.
+        if (node.id.name === 'handler' && node.init) {
+          const initializer = node.init
+          result.prependLeft(initializer.range[0], 'trackMethodCalls(')
+          result.appendRight(initializer.range[1], ', baseObject)')
+          proxyHandlerFound = true
+        }
+      },
     },
   })
+
+  if (!notifyUpdateFound) {
+    console.warn('[valtio-kit] Could not find `notifyUpdate` function')
+    return null
+  }
+
+  if (!proxyHandlerFound) {
+    console.warn('[valtio-kit] Could not find `handler` variable')
+    return null
+  }
 
   // By tracking method calls, we can coalesce set/delete operations.
   result.append(dedent/* js */ `
