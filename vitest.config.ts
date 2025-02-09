654d712777de1db4eb94695adda99a1b73a17074
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-08 19:10:01 -0500

    chore: fix vitest not finding tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     isolate: false,
+    include: ['test/**/*.test.{ts,tsx}'],
     exclude: ['**/vendor/**', 'node_modules'],
     setupFiles: ['./test/setup.ts'],
     server: {
